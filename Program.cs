bd0101e49180c83a9ed8aeccd7ca86822a363c1b
croftn <nyatcroft@gmail.com>
2024-01-31 13:48:58 -0700

    TicTacTools

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Mission4Assignment;
+
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
 // Noah Hicks did this. DO NOT DELETE I WILL DEMOTE YOU
