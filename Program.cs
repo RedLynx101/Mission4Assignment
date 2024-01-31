f2acb2039340f30512d9fe340cbe8c210baa6f2f
maurineiko <nohaskett@gmail.com>
2024-01-31 13:45:50 -0700

    testing

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // Noah Hicks did this. DO NOT DELETE I WILL DEMOTE YOU
+
+// Noah 2.0 did this
