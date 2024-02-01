251b1dcfef23242752ca1a16783d1b0fec7f519d
maurineiko <nohaskett@gmail.com>
2024-02-01 14:32:10 -0700

    Noah Haskett's poorly made tic tac toe skeleton.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,54 @@
-﻿using Mission4Assignment;
+﻿using System.Runtime.InteropServices;
+using Mission4Assignment;
 
 TicTacTools ttt = new TicTacTools();
 
+string[] gameBoardArray = new string[9];
+
+string gameBoard = "";
+string p1Position = "";
+string positionsPlayed = "";
+bool gameOver = false;
+
+// Welcome the user to the game
+
 Console.WriteLine("Welcome to our tic tac toe game!");
+Console.WriteLine("Loading the playing field");
+Thread.Sleep(300);
+Console.Write(".");
+Thread.Sleep(300);
+Console.Write(".");
+Thread.Sleep(300);
+Console.Write(".");
 
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
 // Noah Hicks did this. DO NOT DELETE I WILL DEMOTE YOU
 
 // Testing the commit and push again.
+
+gameBoard = ttt.DisplayBoard(gameBoardArray);
+
+Console.WriteLine(gameBoard);
+
+do
+{
+    // Get the user's play
+    Console.WriteLine("Please enter your desired position from the array Player 1");
+
+    do
+    {
+        p1Position = Console.ReadLine();
+
+    } while (!ttt.ValidPosition(p1Position, positionsPlayed));
+
+    positionsPlayed += p1Position;
+
+    int positionInt = int.Parse(p1Position);
+
+    gameBoardArray[positionInt - 1] = "X";
+
+    Console.WriteLine(string.Join(", ", gameBoardArray));
+
+    Console.WriteLine("Please select your position Player 2");
+
+} while (!gameOver);
--- TicTacTools.cs
+++ TicTacTools.cs
@@ -8,5 +8,47 @@ namespace Mission4Assignment
 {
     internal class TicTacTools
     {
+        public string DisplayBoard(string[] board)
+        {
+            string result = "";
+
+            result = "\n 1 | 2 | 3 \n" +
+                     "----------- \n" +
+                     " 4 | 5 | 6 \n" +
+                     "----------- \n" +
+                     " 7 | 8 | 9 \n";
+
+            return result;
+        }
+
+        public bool ValidPosition(string position, string positionString)
+        {
+            bool result = true; // Default to valid guess
+
+            if (position.Length != 1) // Check for only one character
+            {
+                Console.WriteLine("Sorry, the position is invalid - must be one number. Try again!");
+                result = false;
+            }
+            else if (!Char.IsDigit(position[0])) // Check to see if the character is a digit
+            {
+                Console.WriteLine("Sorry, the position is invalid - must be a digit. Try again!");
+                result = false;
+            }
+            else if (positionString.Contains(position)) // Check to see if the letter has already been guessed
+            {
+                Console.WriteLine("Sorry, the position is invalid - that position has already been played Try again!");
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool CheckWinHorizontal()
+        {
+            bool result = false;
+
+            return result;
+        }
     }
 }
