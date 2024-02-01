3c5b1fe951700096048f83d16a0ae782263ff61e
croftn <nyatcroft@gmail.com>
2024-02-01 15:33:16 -0700

    Everything

--- Program.cs
+++ Program.cs
@@ -2,14 +2,60 @@
 
 TicTacTools ttt = new TicTacTools();
 
+string player1 = "";
+string player2 = "";
+int playerTurn = 0;
+string playerName = "";
+string token = "";
+bool isContinuing = true;
+string[] game = new string[9];
+int position = 0;
+
 Console.WriteLine("Welcome to our tic tac toe game!");
 
 
-internal class Program
+Console.WriteLine("Please enter player 1's name: ");
+player1 = Console.ReadLine();
+Console.WriteLine("Please enter player 2's name: ");
+player2 = Console.ReadLine();  
+
+for (int i = 0; i < game.Length; i++)
 {
-    private static void Main(string[] args)
+    if (playerTurn == 0)
     {
-        Console.WriteLine("Hello, World!");
+        playerName = player1;
+        token = "X";
+    }
+    if (playerTurn == 1)
+    {
+        playerName = player2;
+        token = "O";
+    }
+
+    while (isContinuing)
+    {
+        Console.WriteLine($"{playerName}, please enter the position number you would like: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out position))
+        {
+            isContinuing = false;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+    isContinuing = true;
+    game[position - 1] = token;
+    Console.WriteLine(ttt.UpdatePrintBoard(game));
+    
+    if (playerTurn == 0)
+    {
+        playerTurn = 1;
+    }
+    else
+    {
+        playerTurn = 0;
     }
 }
-// Noah Hicks did this. DO NOT DELETE I WILL DEMOTE YOU
